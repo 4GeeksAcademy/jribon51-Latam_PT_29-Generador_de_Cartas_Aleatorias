d8d0037f86e2dd764422f9c2c257eb27f762fc35
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-03-23 03:34:26 +0000

    [se realizan cambios en html,js,css] se realiza toda la estructura de la carta html  y tambien se realiza css para para cambiar los  estilos sin usar bootstrap y por útimo se hace  la lógica js hasta el momento haciendo los cambios  utilizando un boton.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let boton = document.querySelector(".btn");
+boton.addEventListener("click", e => {
+  let palo = ["♦", "♥", "♠", "♣"];
+  let numero = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let resultadoPalo = palo[Math.floor(Math.random() * palo.length)];
+  let resultadoNumero = numero[Math.floor(Math.random() * numero.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let domPalo = document.querySelectorAll(".palo");
+
+  domPalo.forEach(elemento => {
+    elemento.innerHTML = resultadoPalo;
+  });
+
+  let domNumero = document.querySelector(".numero");
+  domNumero.innerHTML = resultadoNumero;
+
+  if (resultadoPalo == "♦" || resultadoPalo == "♥") {
+    let color = document.querySelectorAll(".color");
+
+    color.forEach(elemento => {
+      elemento.style.color = "#FF0000";
+    });
+  } else {
+    let color = document.querySelectorAll(".color");
+
+    color.forEach(elemento => {
+      elemento.style.color = "#000000";
+    });
+  }
+  console.log(resultadoPalo);
+});
